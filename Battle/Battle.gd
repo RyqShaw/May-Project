cbfc20e0134571596cc2e7dcab6ac18a8ec4174a
RyqShaw <sharyq.siddiqi@gmail.com>
2022-05-10 11:50:10 -0500

    Implementation of Basic Turn Manager & Player Stats Resources

---
+++ Battle/Battle.gd
@@ -0,0 +1,7 @@
+extends Node
+
+const player = preload("res://Battle/Player/PlayerStats.tres")
+const turnManager = preload("res://Battle/TurnManager.tres")
+
+func _ready():
+	pass # Replace with function body.
