6d0a01d051e087ac5c4fc67c8e5f553f62613902
RyqShaw <sharyq.siddiqi@gmail.com>
2022-05-19 14:47:46 -0500

    Implemented Breakdance, fixed water, refactored card picker

--- GlobalInfo.gd
+++ GlobalInfo.gd
@@ -5,7 +5,8 @@ const cardHandler = preload("res://Battle/Cards/CardHandler.tres")
 signal deck_changed
 
 var rooms = 1
-var deckSize = 10 setget set_deck_size
+var deckSize = 10 setget set_deck_size 
+
 
 func set_deck_size(value):
 	deckSize = cardHandler.deck.size()
@@ -24,14 +25,11 @@ func update_master_vol(vol):
 	AudioServer.set_bus_volume_db(0, vol)
 	Save.game_data.Master = vol
 	Save.save_data()
-	print(Save.game_data.Master)
 func update_music_vol(vol):
 	AudioServer.set_bus_volume_db(1, vol)
 	Save.game_data.Music = vol
 	Save.save_data()
-	print(Save.game_data.Music)
 func update_sfx_vol(vol):
 	AudioServer.set_bus_volume_db(2, vol)
 	Save.game_data.Sfx = vol
 	Save.save_data()
-	print(Save.game_data.Sfx)
--- save.gd
+++ save.gd
@@ -12,15 +12,15 @@ func load_data():
 	if not file.file_exists(SAVEFILE):
 		game_data = {
 			"fullscreen": false,
-			"Master": -10.0,
-			"Sfx": -10.0,
+			"Master": 0.0,
+			"Sfx": -15.0,
 			"Music": -10.0
 		}
 		save_data()
 	file.open(SAVEFILE, File.READ)
 	game_data = file.get_var()
 	file.close()
-
+	
 func save_data():
 	var file = File.new()
 	file.open(SAVEFILE, File.WRITE)
