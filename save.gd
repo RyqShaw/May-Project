1d61136a6a1df186ce15764f5e8d795f7ab061b5
RyqShaw <sharyq.siddiqi@gmail.com>
2022-05-20 13:40:14 -0500

    Release Version Made, Bug Fixing

--- save.gd
+++ save.gd
@@ -11,6 +11,7 @@ func load_data():
 	var file = File.new()
 	if not file.file_exists(SAVEFILE):
 		game_data = {
+			"first_time" : true,
 			"fullscreen": false,
 			"Master": 0.0,
 			"Sfx": -15.0,
