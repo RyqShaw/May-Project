27946b3216942aee32562c9eb4f16bacdbffd2a1
Simber1842 <simber1842@gmail.com>
2022-05-26 14:06:42 -0500

    Duet Card

--- Battle/Cards/Duet.gd
+++ Battle/Cards/Duet.gd
@@ -1,2 +1,10 @@
 extends "res://Battle/Cards/BaseCard/CardBase.gd"
 
+const cardHandler = preload("res://Battle/Cards/CardHandler.tres")
+
+func action():
+	var battle = battleUnits.Battle
+	if battle != null:
+		var last_card = cardHandler.last_card
+		cardHandler.append_last_card(last_card)
+	
