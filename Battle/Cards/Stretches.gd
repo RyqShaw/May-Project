58fb9c7c5e526a7dd533c3cb1252f61aa63f54af
Simber1842 <simber1842@gmail.com>
2022-05-27 14:22:56 -0500

    Girlfriend added

--- Battle/Cards/Stretches.gd
+++ Battle/Cards/Stretches.gd
@@ -7,7 +7,7 @@ func action():
 	var battle = battleUnits.Battle
 	if player != null:
 		SoundManager.play_sound(load("res://SoundAffects/powerUp.wav"))
-		if player.weakenEnemy == false:
+		if player.enemyWeakened == false:
 			player.damage_mod = 1.25
 		var c = stretch.instance()
 		battle.get_node("PlayerCounters").add_child(c)
