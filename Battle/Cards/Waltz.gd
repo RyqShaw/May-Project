63840a4a18abb1f483ba6cc6395179673d28e9ad
Simber1842 <simber1842@gmail.com>
2022-05-30 12:58:02 -0500

    Bug fixes

--- Battle/Cards/Waltz.gd
+++ Battle/Cards/Waltz.gd
@@ -1,8 +1,14 @@
 extends "res://Battle/Cards/BaseCard/CardBase.gd"
 
+const waltzIndicator = preload("res://Battle/Player/WaltzBuff.tscn")
+var buff
+
 func action():
 	var player = battleUnits.Player
 	var enemy = battleUnits.Enemy
 	if player != null:
 		SoundManager.play_sound(load("res://SoundAffects/powerUp.wav"))
 		enemy.setDmgBoost(2)
+		var indicator = waltzIndicator.instance()
+		battleUnits.Battle.find_node("PlayerBuffs").add_child(indicator)
+		buff = indicator
